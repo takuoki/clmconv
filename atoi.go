be028fa544248af34707ef7981bad5e8c6b08538
Takuo Oki <takuo.liamg@gmail.com>
2022-08-13 17:07:23 +0900

    implement convert options (#10)
    * implement convert options
    
    * circleci -> github actions
    
    * add codecov
    
    Co-authored-by: takuo.oki <takuo.oki@optim.co.jp>

--- atoi.go
+++ atoi.go
@@ -1,4 +1,3 @@
-// Package clmconv converts spreadsheet column alphabet to integer and vice versa.
 package clmconv
 
 import (
@@ -7,26 +6,37 @@ import (
 
 // Atoi converts alphabet to number.
 func Atoi(s string) (int, error) {
+	return defaultConverter.Atoi(s)
+}
+
+// Atoi converts alphabet to number with converter.
+func (c *Converter) Atoi(s string) (int, error) {
 	if s == "" {
 		return 0, errors.New("argument is empty string")
 	}
 	var r int
-	for i, c := range s {
-		if 'A' <= c && c <= 'Z' {
-			r += (int(c) - 64) * pow26(len(s)-i-1)
-		} else if 'a' <= c && c <= 'z' {
-			r += (int(c) - 96) * pow26(len(s)-i-1)
+	for i, char := range s {
+		if 'A' <= char && char <= 'Z' {
+			r += (int(char) - runeOffsetUpper) * pow26(len(s)-i-1)
+		} else if 'a' <= char && char <= 'z' {
+			r += (int(char) - runeOffsetLower) * pow26(len(s)-i-1)
 		} else {
 			return 0, errors.New("must not contain non-alphabetic characters")
 		}
 	}
-	return r - 1, nil
+	return r - c.offset, nil
 }
 
 // MustAtoi converts alphabet to number.
 // In case of invalid argument, throws panic.
 func MustAtoi(s string) int {
-	r, err := Atoi(s)
+	return defaultConverter.MustAtoi(s)
+}
+
+// MustAtoi converts alphabet to number with converter.
+// In case of invalid argument, throws panic.
+func (c *Converter) MustAtoi(s string) int {
+	r, err := c.Atoi(s)
 	if err != nil {
 		panic(err.Error())
 	}
--- atoi_test.go
+++ atoi_test.go
@@ -7,34 +7,52 @@ import (
 	"github.com/takuoki/clmconv"
 )
 
-func TestAtoi(t *testing.T) {
+func TestConverter_Atoi(t *testing.T) {
+	dc := clmconv.New()
+	cc := clmconv.New(clmconv.WithStartFromOne())
+
 	cases := []struct {
+		conv     *clmconv.Converter
 		a        string
 		expected int
 		err      string
 	}{
-		// success
-		{a: "A", expected: 0},
-		{a: "B", expected: 1},
-		{a: "Z", expected: 25},
-		{a: "AA", expected: 26},
-		{a: "ZZ", expected: 701},
-		{a: "ABC", expected: 730},
-		{a: "ABCDE", expected: 494264},
-		{a: "FXSHRXX", expected: math.MaxInt32},
-		{a: "CRPXNLSKVLJFHG", expected: math.MaxInt64 - 1},
-		{a: "CRPXNLSKVLJFHH", expected: math.MaxInt64},
-		{a: "a", expected: 0},
-		{a: "AbCdE", expected: 494264},
+		// success (default converter)
+		{conv: dc, a: "A", expected: 0},
+		{conv: dc, a: "B", expected: 1},
+		{conv: dc, a: "Z", expected: 25},
+		{conv: dc, a: "AA", expected: 26},
+		{conv: dc, a: "ZZ", expected: 701},
+		{conv: dc, a: "ABC", expected: 730},
+		{conv: dc, a: "ABCDE", expected: 494264},
+		{conv: dc, a: "FXSHRXX", expected: math.MaxInt32},
+		{conv: dc, a: "CRPXNLSKVLJFHG", expected: math.MaxInt64 - 1},
+		{conv: dc, a: "CRPXNLSKVLJFHH", expected: math.MaxInt64},
+		{conv: dc, a: "a", expected: 0},
+		{conv: dc, a: "AbCdE", expected: 494264},
+
+		// success (custom converter)
+		{conv: cc, a: "A", expected: 1},
+		{conv: cc, a: "B", expected: 2},
+		{conv: cc, a: "Z", expected: 26},
+		{conv: cc, a: "AA", expected: 27},
+		{conv: cc, a: "ZZ", expected: 702},
+		{conv: cc, a: "ABC", expected: 731},
+		{conv: cc, a: "ABCDE", expected: 494265},
+		{conv: cc, a: "FXSHRXW", expected: math.MaxInt32},
+		{conv: cc, a: "CRPXNLSKVLJFHF", expected: math.MaxInt64 - 1},
+		{conv: cc, a: "CRPXNLSKVLJFHG", expected: math.MaxInt64},
+		{conv: cc, a: "a", expected: 1},
+		{conv: cc, a: "AbCdE", expected: 494265},
 
 		// error
-		{a: "", err: "argument is empty string"},
-		{a: "1", err: "must not contain non-alphabetic characters"},
-		{a: "AB!", err: "must not contain non-alphabetic characters"},
+		{conv: dc, a: "", err: "argument is empty string"},
+		{conv: dc, a: "1", err: "must not contain non-alphabetic characters"},
+		{conv: dc, a: "AB!", err: "must not contain non-alphabetic characters"},
 	}
 
 	for _, c := range cases {
-		r, err := clmconv.Atoi(c.a)
+		r, err := c.conv.Atoi(c.a)
 		if c.err == "" {
 			if err != nil {
 				t.Errorf("error must not occur: %s", err.Error())
@@ -51,23 +69,23 @@ func TestAtoi(t *testing.T) {
 	}
 }
 
-func TestMustAtoi(t *testing.T) {
-	// success
-	func() {
+func TestConverter_MustAtoi(t *testing.T) {
+	dc := clmconv.New()
+
+	t.Run("success", func(t *testing.T) {
 		defer func() {
 			if e := recover(); e != nil {
 				t.Errorf("panic must not occur: %v", e)
 			}
 		}()
-		clmconv.MustAtoi("ABC")
-	}()
-	// error
-	func() {
+		dc.MustAtoi("ABC")
+	})
+	t.Run("panic", func(t *testing.T) {
 		defer func() {
 			if e := recover(); e == nil {
 				t.Errorf("panic must occur")
 			}
 		}()
-		clmconv.MustAtoi("AB!")
-	}()
+		dc.MustAtoi("AB!")
+	})
 }
