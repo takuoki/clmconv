ddaa6333a394be5ade92359796aa4ad78b69a330
takuoki <takuo.liamg@gmail.com>
2025-10-19 18:45:40 +0900

    add overflow check

--- atoi.go
+++ atoi.go
@@ -2,6 +2,7 @@ package clmconv
 
 import (
 	"errors"
+	"math"
 )
 
 // Atoi converts alphabet to number.
@@ -14,17 +15,42 @@ func (c *Converter) Atoi(s string) (int, error) {
 	if s == "" {
 		return 0, errors.New("argument is empty string")
 	}
-	var r int
+
+	// Length check for potential overflow
+	// 14 characters is the practical limit for int64
+	if len(s) > 14 {
+		return 0, errors.New("integer overflow")
+	}
+
+	r := -c.offset
 	for i, char := range s {
+		var charValue int
 		if 'A' <= char && char <= 'Z' {
-			r += (int(char) - runeOffsetUpper) * pow26(len(s)-i-1)
+			charValue = int(char) - runeOffsetUpper
 		} else if 'a' <= char && char <= 'z' {
-			r += (int(char) - runeOffsetLower) * pow26(len(s)-i-1)
+			charValue = int(char) - runeOffsetLower
 		} else {
 			return 0, errors.New("must not contain non-alphabetic characters")
 		}
+
+		power := pow26(len(s) - i - 1)
+
+		// Check for multiplication overflow
+		if charValue > 0 && power > math.MaxInt/charValue {
+			return 0, errors.New("integer overflow")
+		}
+
+		product := charValue * power
+
+		// Check for addition overflow
+		if r > math.MaxInt-product {
+			return 0, errors.New("integer overflow")
+		}
+
+		r += product
 	}
-	return r - c.offset, nil
+
+	return r, nil
 }
 
 // MustAtoi converts alphabet to number.
